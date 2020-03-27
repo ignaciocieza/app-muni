fe2668272bc91167565d21db21ab1efab8e69b4f
ignacio <ignaciocieza@hotmail.com>
2020-03-26 21:18:28 -0300

    mod server

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const compression = require('compression');
 const port = process.env.PORT || 3001;
+
 
 server.use(middlewares);
 server.use(router);
+server.use(compression);
 
 server.listen(port);
