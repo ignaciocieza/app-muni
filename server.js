2f05738372e7f847d089a8e833e67ff325547672
ignacio <ignaciocieza@hotmail.com>
2020-07-30 16:04:02 -0300

    cambio host

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.listen(port, (error) => {
 
 try {
     const config = {
-        host: process.env.DB_HOST_TEST,
+        host: process.env.DB_HOST_PROD,
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
